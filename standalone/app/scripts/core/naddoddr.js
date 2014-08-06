c8f8bec06503d4027e2d47e027141583fccc177b
Raul Tierno <raultm@gmail.com>
2014-08-06 07:34:35 +0200

    JSCORE - Start the JSCORE of Naddoddr

---
+++ standalone/app/scripts/core/naddoddr.js
@@ -0,0 +1,14 @@
+var naddoddr = {};
+
+(function(naddoddr){
+    'use strict';
+    naddoddr.Adventure = function(){
+        this.name = 'La Antilla';
+    };
+
+    var adventure = new naddoddr.Adventure();
+    console.log(adventure.name);
+
+})(naddoddr);
+
+
