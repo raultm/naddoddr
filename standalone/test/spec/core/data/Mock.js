20b65618b1fc2f086239098645b46fcea631e5e8
Raul Tierno <raultm@gmail.com>
2014-08-09 16:53:57 +0200

    CORE:DATA - Adventure.parseObject Empty Object

--- standalone/test/spec/core/data/Mock.js
+++ standalone/test/spec/core/data/Mock.js
@@ -19,11 +19,18 @@ describe('Core:Data Mock', function () {
         expect(naddoddr.Mock.Adventure.adventures[0]).toBe(naddoddr.Mock.Adventure.basic);
     });
 
-    it('should have a function to parse Object to Adventure', function(){
+    it('should have a function to parse Object to Adventure (Empty Object)', function(){
+        var basicObject = {};
+        var basicAdventure = new naddoddr.Adventure(basicObject);
+        expect(naddoddr.Mock.Adventure.parseObject(basicObject)).toEqual(basicAdventure);
+    });
+
+    it('should have a function to parse Object to Adventure (Basic Object)', function(){
         var basicObject = naddoddr.Mock.Adventure.basic;
         var basicAdventure = new naddoddr.Adventure(basicObject);
         expect(naddoddr.Mock.Adventure.parseObject(basicObject)).toEqual(basicAdventure);
     });
+
 
     it('should return all Adventures if findAll', function () {
         expect(naddoddr.Mock.Adventure.findAll()).toBe(naddoddr.Mock.Adventure.adventures);
