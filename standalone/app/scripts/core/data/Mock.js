e61f72860c1e9ca0b0360ffcbd6d846eea7e36a8
Raul Tierno <raultm@gmail.com>
2014-08-09 13:04:41 +0200

    CORE:DATA - Mock - Creating mock Data and functions

--- standalone/app/scripts/core/data/Mock.js
+++ standalone/app/scripts/core/data/Mock.js
@@ -1,9 +1,20 @@
 'use strict';
 
 (function(window, naddoddr, undefined){
-    naddoddr.Mock = {
-        'Adventure': {
-            'basic': {}
-        }
+    naddoddr.Mock = {};
+    naddoddr.Mock.Adventure = {};
+    naddoddr.Mock.Adventure.basic = {
+        'name': 'La Antilla',
+        'description': 'Primer Baño de Duna en el Mar'
     };
+
+    naddoddr.Mock.Adventure.adventures = [
+        naddoddr.Mock.Adventure.basic
+    ];
+
+    naddoddr.Mock.Adventure.findAll = function(){
+        return naddoddr.Mock.Adventure.adventures;
+    }
+
+
 })(window, naddoddr);
--- standalone/test/spec/core/data/Mock.js
+++ standalone/test/spec/core/data/Mock.js
@@ -9,7 +9,17 @@ describe('Core:Data Mock', function () {
     it('should have a basic adventure ', function () {
         expect(naddoddr.Mock.Adventure.basic).toBeDefined();
         expect(naddoddr.Mock.Adventure.basic.name).toBe('La Antilla');
-        expect(naddoddr.Mock.Adventure.basic.description).toBe('Primer Baño de Duna en la Playa');
+        expect(naddoddr.Mock.Adventure.basic.description).toBe('Primer Baño de Duna en el Mar');
+        expect(naddoddr.Mock.Adventure.adventures).toBeDefined();
+    });
+
+    it('should have an array of adventures to provide', function () {
+        expect(naddoddr.Mock.Adventure.adventures).toBeDefined();
+        expect(naddoddr.Mock.Adventure.adventures[0]).toBe(naddoddr.Mock.Adventure.basic);
+    });
+
+    it('should return all Adventures if findAll', function () {
+        expect(naddoddr.Mock.Adventure.findAll()).toBe(naddoddr.Mock.Adventure.adventures);
     });
 
 });
