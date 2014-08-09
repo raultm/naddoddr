06898cc4b103b05c83be9067729219d2bd83ba55
Raul Tierno <raultm@gmail.com>
2014-08-09 23:07:24 +0200

    APP - Fix Main References in Tests

--- standalone/test/spec/controllers/adventures.js
+++ standalone/test/spec/controllers/adventures.js
@@ -1,17 +1,17 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: AdventuresCtrl', function () {
 
   // load the controller's module
   beforeEach(module('naddoddrApp'));
 
-  var   MainCtrl,
+  var   AdventuresCtrl,
         scope;
 
   // Initialize the controller and a mock scope
     beforeEach(inject(function ($controller, $rootScope) {
         scope = $rootScope.$new();
-        MainCtrl = $controller('MainCtrl', {
+        AdventuresCtrl = $controller('AdventuresCtrl', {
           $scope: scope
         });
     }));
