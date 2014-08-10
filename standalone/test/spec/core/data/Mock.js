0e4bfbde4f6d7192777e07830cbf686827d05517
Raul Tierno <raultm@gmail.com>
2014-08-10 17:34:31 +0200

    CORE:DATA - Mock - Create Adventure with two stages to test

--- standalone/test/spec/core/data/Mock.js
+++ standalone/test/spec/core/data/Mock.js
@@ -6,56 +6,79 @@ describe('Core:Data Mock', function () {
         expect(naddoddr.Mock).toBeDefined();
     });
 
-    it('should have a basic adventure ', function () {
-        expect(naddoddr.Mock.Adventure.basic).toBeDefined();
-        expect(naddoddr.Mock.Adventure.basic._id).toBe(1);
-        expect(naddoddr.Mock.Adventure.basic.name).toBe('La Antilla');
-        expect(naddoddr.Mock.Adventure.basic.description).toBe('Vacaciones Verano 2014');
-        expect(naddoddr.Mock.Adventure.basic.stages.length).toBe(1);
-        expect(naddoddr.Mock.Adventure.basic.stages[0].name).toBe('La Antilla');
-        expect(naddoddr.Mock.Adventure.basic.stages[0].content).toBe('Duna se baña por primera vez en el Mar');
-        expect(naddoddr.Mock.Adventure.basic.stages[0].geopoint).toEqual({'lat': 37.2071, 'lng': -7.2091});
-        expect(naddoddr.Mock.Adventure.adventures).toBeDefined();
+    describe('As Data Provider', function(){
+        it('should have a function to parse Object to Adventure (Empty Object)', function(){
+            var basicObject = {};
+            var basicAdventure = new naddoddr.Adventure(basicObject);
+            expect(naddoddr.Mock.Adventure.parseObject(basicObject)).toEqual(basicAdventure);
+        });
+
+        it('should have a function to parse Object to Adventure (Basic Object)', function(){
+            var basicObject = naddoddr.Mock.Adventure.basic;
+            var basicAdventure = new naddoddr.Adventure(basicObject);
+            expect(naddoddr.Mock.Adventure.parseObject(basicObject)).toEqual(basicAdventure);
+        });
+
+        it('should have a function to parse Objects to Adventures', function(){
+            var basicObjects = [{}, naddoddr.Mock.Adventure.basic];
+            var basicAdventures = [
+                new naddoddr.Adventure(basicObjects[0]),
+                new naddoddr.Adventure(basicObjects[1])
+            ]
+            expect(naddoddr.Mock.Adventure.parseObjects(basicObjects)).toEqual(basicAdventures);
+        });
+
+        it('should return all Adventures if findAll', function () {
+            var adventures = naddoddr.Mock.Adventure.parseObjects(naddoddr.Mock.Adventure.adventures);
+            expect(naddoddr.Mock.Adventure.findAll()).toEqual(adventures);
+        });
+
+        it('should return basic adventure if ask for Id 1', function () {
+            var adventure = naddoddr.Mock.Adventure.parseObject(naddoddr.Mock.Adventure.basic);
+            expect(naddoddr.Mock.Adventure.findById(1)).toEqual(adventure);
+        });
+
+        it('should return false if asked by id tha doesnt exist', function () {
+            expect(naddoddr.Mock.Adventure.findById(1234)).toBe(false);
+        });
     });
 
-    it('should have an array of adventures to provide', function () {
-        expect(naddoddr.Mock.Adventure.adventures).toBeDefined();
-        expect(naddoddr.Mock.Adventure.adventures[0]).toBe(naddoddr.Mock.Adventure.basic);
+    describe('Adventures Mocked', function(){
+        it('should have an array of adventures to provide', function () {
+            expect(naddoddr.Mock.Adventure.adventures).toBeDefined();
+            expect(naddoddr.Mock.Adventure.adventures.length).toBe(1);
+            expect(naddoddr.Mock.Adventure.adventures[0]).toBe(naddoddr.Mock.Adventure.basic);
+        });
     });
 
-    it('should have a function to parse Object to Adventure (Empty Object)', function(){
-        var basicObject = {};
-        var basicAdventure = new naddoddr.Adventure(basicObject);
-        expect(naddoddr.Mock.Adventure.parseObject(basicObject)).toEqual(basicAdventure);
+    describe('AntillaAdventure', function(){
+        it('should have a basic adventure ', function () {
+            expect(naddoddr.Mock.Adventure.basic).toBeDefined();
+            expect(naddoddr.Mock.Adventure.basic._id).toBe(1);
+            expect(naddoddr.Mock.Adventure.basic.name).toBe('La Antilla');
+            expect(naddoddr.Mock.Adventure.basic.description).toBe('Vacaciones Verano 2014');
+            expect(naddoddr.Mock.Adventure.basic.stages.length).toBe(1);
+            expect(naddoddr.Mock.Adventure.basic.stages[0].name).toBe('La Antilla');
+            expect(naddoddr.Mock.Adventure.basic.stages[0].content).toBe('Duna se baña por primera vez en el Mar');
+            expect(naddoddr.Mock.Adventure.basic.stages[0].geopoint).toEqual({'lat': 37.2071, 'lng': -7.2091});
+            expect(naddoddr.Mock.Adventure.adventures).toBeDefined();
+        });
     });
 
-    it('should have a function to parse Object to Adventure (Basic Object)', function(){
-        var basicObject = naddoddr.Mock.Adventure.basic;
-        var basicAdventure = new naddoddr.Adventure(basicObject);
-        expect(naddoddr.Mock.Adventure.parseObject(basicObject)).toEqual(basicAdventure);
-    });
-
-    it('should have a function to parse Objects to Adventures', function(){
-        var basicObjects = [{}, naddoddr.Mock.Adventure.basic];
-        var basicAdventures = [
-            new naddoddr.Adventure(basicObjects[0]),
-            new naddoddr.Adventure(basicObjects[1])
-        ]
-        expect(naddoddr.Mock.Adventure.parseObjects(basicObjects)).toEqual(basicAdventures);
-    });
-
-    it('should return all Adventures if findAll', function () {
-        var adventures = naddoddr.Mock.Adventure.parseObjects(naddoddr.Mock.Adventure.adventures);
-        expect(naddoddr.Mock.Adventure.findAll()).toEqual(adventures);
-    });
-
-    it('should return basic adventure if ask for Id 1', function () {
-        var adventure = naddoddr.Mock.Adventure.parseObject(naddoddr.Mock.Adventure.basic);
-        expect(naddoddr.Mock.Adventure.findById(1)).toEqual(adventure);
-    });
-
-    it('should return false if asked by id tha doesnt exist', function () {
-        expect(naddoddr.Mock.Adventure.findById(1234)).toBe(false);
+    describe('CereceraAdventure', function(){
+        it('should have a twostages adventure ', function () {
+            expect(naddoddr.Mock.Adventure.twostages).toBeDefined();
+            expect(naddoddr.Mock.Adventure.twostages._id).toBe(2);
+            expect(naddoddr.Mock.Adventure.twostages.name).toBe('Cerecera y Kayaks');
+            expect(naddoddr.Mock.Adventure.twostages.description).toBe('Valle del Jerte, Cerezas, Familia y Kayaks');
+            expect(naddoddr.Mock.Adventure.twostages.stages.length).toBe(2);
+            expect(naddoddr.Mock.Adventure.twostages.stages[0].name).toBe('Cabrero');
+            expect(naddoddr.Mock.Adventure.twostages.stages[0].content).toBe('La cuadrillas a por cerezas, Duna ruta por el pueblo');
+            expect(naddoddr.Mock.Adventure.twostages.stages[0].geopoint).toEqual({'lat': 40.112689, 'lng': -5.891976});
+            expect(naddoddr.Mock.Adventure.twostages.stages[1].name).toBe('Kayaks');
+            expect(naddoddr.Mock.Adventure.twostages.stages[1].content).toBe('Kayaks con Guia2 en el Embalse de Plasencia');
+            expect(naddoddr.Mock.Adventure.twostages.stages[1].geopoint).toEqual({'lat': 40.077683, 'lng': -6.013669});
+        });
     });
 
 });
